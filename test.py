cca1a96161201ec433384b9b9db1690b38c1da2d
ancysnovee <ancysnoveem@gamil.com>
2025-03-26 02:15:08 -0700

    updated

--- test.py
+++ test.py
@@ -4,11 +4,11 @@ from course import website
 def test_website():
    
     result = website(1, selected_courses=["maths", "english"])
-    assert result == "you have enrolled successfully in the following course:\n['maths', 'english']", f"Test failed: {result}"
+    assert result == "you have enrolled successfully in the following course:\n['maths', 'english']"
 
     
     result = website(2, new_course="history")
-    assert result == "the course has been added successfully: ['maths', 'kannada', 'english', 'social', 'science', 'history']", f"Test failed: {result}"
+    assert result == "the course has been added successfully: ['maths', 'kannada', 'english', 'social', 'science', 'history']"
 
 
 
